c5d426ec357daebbaf2f54ca29d19f74b4fdfc31
Sébastien Drouyer <drouyer@novius.com>
2013-10-22 14:23:39 +0200

    updated how it save

--- htdocs/save_data.php
+++ htdocs/save_data.php
@@ -12,15 +12,15 @@ if (isset($_GET['v'])) {
     $set = new \Novius\Sieste\Model_Set();
     $set->set_date = Date::forge()->format('mysql');
     $set->set_nb_sleeping = 0;
-    $set->set_type = $_GET['t'] == 'b' ? 'boom' : 'periodic';
+    $set->set_type = $_GET['t'] == '1' ? 'boom' : 'periodic';
     $set->save();
 
     foreach ($values as $key => $value) {
         $data = new \Novius\Sieste\Model_Data();
         $data->data_set_id = $set->set_id;
         $data->data_capt_id = $key;
-        $data->data_capt_value = $value;
-        $data->data_capt_average = $averages[$key];
+        $data->data_capt_value = $value / 100;
+        $data->data_capt_average = $averages[$key] / 100;
         $data->save();
     }
 
